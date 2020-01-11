e3b426440b53b119dc3f06d8018491a1011b10d9
nafies1 <ghuroba.muslim@gmail.com>
2020-01-11 12:26:48 +0700

    add update todo in route todos. Make it works

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -19,6 +19,35 @@ class TodoController{
                 next(err)
             })
     }
+
+    static showAll(req, res, next){
+        Todo.find({})
+            .then(todos=>{
+                console.log(todos)
+                res.status(200).json(todos)
+            })
+            .catch(err=>{
+                next(err)
+            })
+    }
+
+    static update(req, res, next){
+        const {id, name, description, status, budget_needed, due_date} = req.body
+        Todo.findByIdAndUpdate(id, { 
+            name,
+            description,
+            status,
+            budget_needed: budget_needed || 0,
+            due_date: due_date || null
+        })
+            .then(todo=>{
+                console.log(todo)
+                res.status(200).json(todo)
+            })
+            .catch(err=>{
+                next(err)
+            })
+    }
 }
 
 module.exports = TodoController
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const Todo = require('../controllers/todos')
 
 router.post('/', Todo.create)
-// router.get('/', Todo.showAll)
-// router.put('/', Todo.update)
+router.get('/', Todo.showAll)
+router.put('/', Todo.update)
 // router.patch('/', Todo.updateStatus)
 // router.delete('/', Todo.delete)
 
