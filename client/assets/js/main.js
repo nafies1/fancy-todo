a3b1947ddca7de1eef766a13792db9c2251a5ca4
nafies1 <ghuroba.muslim@gmail.com>
2020-01-13 01:01:48 +0700

    Make main.js for login and register page works

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,6 +1,8 @@
 function onSignIn(googleUser) {
     const id_token = googleUser.getAuthResponse().id_token;
-
+    console.log(id_token);
+    
+    /*
     $.ajax({
         method : 'POST',
         url : 'http://localhost:3000/user/google-sign',
@@ -17,7 +19,7 @@ function onSignIn(googleUser) {
       })
       .fail( err => {
         console.log(err);
-      })
+      }) */
   }
 
 function signOut() {
@@ -27,4 +29,79 @@ function signOut() {
     });
 }
 
+function login(){
 
+  $('#loginButton').click(event =>{
+    console.log(event);
+    event.preventDefault()
+    console.log($('#email').val());
+    console.log($('#password').val());
+    if ($('#remember').is(":checked")){
+      console.log('remember');
+      
+    } else console.log('don\'t remember');
+    
+    $.ajax({
+        method : 'POST',
+        url : `http://localhost:3000/user/login`,
+        data : {
+          email : $('#email').val(),
+          password : $('#password').val()
+        },
+        beforeSend: function () {
+         $('#repo-container').html(`<div class="d-flex justify-content-center p-3">
+         <div class="spinner-border" role="status">
+           <span class="sr-only">Loading...</span>
+         </div>
+       </div>`);
+      }
+    })
+      .done(function (data) {
+        console.log(data);
+       
+      })
+      .fail(function (err) {
+         console.log(err)
+      });
+  }) // loginButton click event
+} // function login
+
+function register(){
+
+  $('#registerButton').click(event =>{
+    console.log(event);
+    event.preventDefault()
+    console.log($('#nameRegister').val());
+    console.log($('#emailRegister').val());
+    console.log($('#passwordRegister').val());
+
+    $.ajax({
+        method : 'POST',
+        url : `http://localhost:3000/user/register`,
+        data : {
+          name : $('#nameRegister').val(),
+          email : $('#emailRegister').val(),
+          password : $('#passwordRegister').val()
+        },
+        beforeSend: function () {
+         $('#repo-container').html(`<div class="d-flex justify-content-center p-3">
+         <div class="spinner-border" role="status">
+           <span class="sr-only">Loading...</span>
+         </div>
+       </div>`);
+      }
+    })
+      .done(function (data) {
+        console.log(data);
+       
+      })
+      .fail(function (err) {
+         console.log(err)
+      });
+  }) // registerButton click event 
+} // function register
+
+$(document).ready(()=>{
+  login()
+  register()
+})
